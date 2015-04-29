d6cb815c1c09b15becd6cb5da2c9fa66774de0a3
kevinfguo <blindspotx2@gmail.com>
2015-04-29 13:28:25 -0400

    More playtesting
    Blah

--- crawler.php
+++ crawler.php
@@ -5,13 +5,19 @@ ini_set('display_errors', true); ini_set('display_startup_errors', true); error_
 class Crawler{
 
 	private $name = '';
+	private $name_arr;
 
 	function setQuery($qname){
 		$name = $qname;
+		$name_arr = explode(" ", $qname);
 		printpre($name);
+		printpre($name_arr);
 	}
 	function crawl($website){
+		if ($website == 'muckrack'){
+			$url = 'http://muckrack.com/search/results?q='
 
+		}
 	}
 }
 ?>
